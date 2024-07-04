e3cc7338d125d420240d32b85b394a9e6d8afbc9
Ravi-Prajapati <ravip@technobrave.com>
2024-07-04 12:42:20 +0530

    * add find View By Id

--- app/src/main/java/com/technobrave/sampleproject/MainActivity.kt
+++ app/src/main/java/com/technobrave/sampleproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.technobrave.sampleproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var editText:EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText?.findViewById<EditText>(R.id.editText)
+
     }
 }
